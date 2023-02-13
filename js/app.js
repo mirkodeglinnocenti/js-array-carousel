c8c6021d0501cbdb1aabdeb36b834401cb9f5d44
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-02-13 09:37:27 +0100

    UP

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 // console.log('Test')
 
 
+
+
+
+
+// array con immagini
+const slides = [
+    './img/01.jpg',
+    './img/02.jpg',
+    './img/03.jpg',
+    './img/04.jpg',
+    './img/05.jpg',
+]
+
 // Prendere le slide da html
 const slideElements = document.getElementsByClassName('slide');
 // console.log(slideElements);
